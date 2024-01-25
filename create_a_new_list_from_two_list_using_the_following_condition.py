552a4bbac2f92d8426f695cfd1606913cf813b1e
James Mikko P. Recario <jamesmikkoprecario@iskolarngbayan.pup.edu.ph>
2024-01-26 06:23:40 +0800

    Program Instructions and Pseudocode

---
+++ create_a_new_list_from_two_list_using_the_following_condition.py
@@ -0,0 +1,8 @@
+# Assignment 5 - Exercise 10
+# Create a new list from two list using the following condition
+# Given two list of numbers, write a program to create a new list such that the new list should contain odd numbers from the first list 
+# and even numbers from the second list.
+
+# Merge the lists
+# Make new two new lists, odd numbers of list 1 and even numbers of list 2
+# Print output
